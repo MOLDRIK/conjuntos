5c245cd88d4bec65724b960f1929f38804c51da3
MOLDRIK <candres15@hotmail.com>
2024-07-03 19:29:55 -0500

    Add files via upload

--- pag1/pag1/pagina/urls.py
+++ pag1/pag1/pagina/urls.py
@@ -19,13 +19,11 @@ Including another URLconf
 # urls.py
 from django.contrib import admin
 from django.urls import path, include
-from polls import views
+from polls import views as polls_views
 #from . import views  # Asegúrate de importar tus vistas
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('polls/', include('polls.urls')),
-    path('', views.index, name='home'),  # Añade esta línea
-]
-
-
+    path('', polls_views.index, name='home')
+ ]
--- pag1/pag1/polls/views.py
+++ pag1/pag1/polls/views.py
@@ -11,3 +11,40 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Bienvenido a la pagina principal")
+
+def formulario(request):
+    return render(request, 'polls/index.html')
+
+def index(request):
+    context = {}
+    if request.method == 'POST':
+        # Capturar los datos del formulario
+        apartamento = request.POST.get('apartamento')
+        nombre = request.POST.get('nombre')
+        apellidos = request.POST.get('apellidos')
+        capital = float(request.POST.get('capital', 0))
+        correo = request.POST.get('correo')
+        cartera = request.POST.get('carteras')
+
+        # Calcular los honorarios y el total
+        honorarios = capital * 0.10
+        total = capital + honorarios
+
+        # Añadir los datos calculados al contexto para mostrarlos en la plantilla
+        context = {
+            'apartamento': apartamento,
+            'nombre': nombre,
+            'apellidos': apellidos,
+            'capital': capital,
+            'honorarios': honorarios,
+            'total': total,
+            'correo': correo,
+            'cartera': cartera,
+        }
+
+        # Opcional: Guardar los datos en la base de datos o realizar alguna acción
+        # Aquí puedes añadir la lógica para guardar los datos en el modelo correspondiente
+
+        # Redirigir a una página de éxito o volver a mostrar el formulario con los datos calculados
+        return render(request, 'polls/index.html', context)
+    return render(request, 'polls/index.html', context)
